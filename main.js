919336f6fb615eb649da2880fbb99b428c980a32
James Draper <jamesdrape@gmail.com>
2015-11-26 14:57:31 +0000

    Desk Tickets Widget - static

---
+++ main.js
@@ -0,0 +1,14 @@
+function openDeskTicket(e) {
+	var $target = $(e.target);
+	var ticketNum = $target.closest('tr.table-row').attr('data-ticketId');
+	var ticketUrl = "https://digitalcrew.teamwork.com/desk/#tickets/" + ticketNum;
+
+	window.open(ticketUrl, '_blank');
+	e.preventDefault();
+}
+
+$(document).ready(function(){
+
+	$('#sectionTicketList').find('tr.table-row').on('click', openDeskTicket);
+
+});
